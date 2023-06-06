85582feafa616383577ac40747c5650948a7781d
Tyler <td20147@gmail.com>
2023-06-06 17:22:01 -0400

    sign up box styled

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import SignUp from "./sign-up/SignUp.jsx";
 import Login from './login/Login.jsx';
+import './App.css';
 
 function App() {
   return (
     <>
       <SignUp />
-      <Login />
     </>
   );
 }
--- frontend/src/pages/sign-up/SignUp.jsx
+++ frontend/src/pages/sign-up/SignUp.jsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import Logout from "../../components/Logout";
 
 function SignUp() {
-
   const handleSignUp = (e) => {
     e.preventDefault();
 
@@ -53,18 +52,21 @@ function SignUp() {
     return false;
   };
 
-
   return (
     <div className="signUp">
       <div className="signUp-container">
-        <h1>Sign Up</h1>
         <form id="signUpForm">
+          <h1>Sign Up</h1>
           <h5>Email</h5>
           <input type="email" name="email" />
           <h5>Password</h5>
           <input type="password" name="password" />
           <h5>Confirm Password</h5>
           <input type="password" name="confirmPassword" />
+          <p>
+            By signing up, you agree to our Terms of Use and Privacy Policy.
+          </p>
+          <p>Already have an account? Sign In</p>
           <button
             type="submit"
             className="signUp-signInButton"
@@ -73,10 +75,8 @@ function SignUp() {
             Sign Up
           </button>
         </form>
-        <p>By signing up, you agree to our Terms of Use and Privacy Policy.</p>
-        <p>Already have an account? Sign In</p>
+        <img src="https://media.istockphoto.com/id/1399426267/photo/serious-young-mixed-race-female-posing-in-trendy-fashionable-clothing-while-sitting-on-a.webp?b=1&s=170667a&w=0&k=20&c=pSYFj8lk1CtfDSYhf5kZOQFM3S6p023xmTVPpjCWuQQ=" alt="happy person" />
       </div>
-      <Logout />
     </div>
   );
 }
