197b3efc8e56f05aca002826ce809fe3ed298957
averydossman <averydossman@gmail.com>
2023-06-06 16:49:05 -0400

    cart quantity icon updates automatically

--- frontend/src/pages/cart/Cart.jsx
+++ frontend/src/pages/cart/Cart.jsx
@@ -1,64 +1,84 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect, useContext } from 'react';
+//import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { Button, List, ListItem, ListItemText, IconButton, Typography } from '@mui/material';
+import CartQuantity from './CartQuantity';
+import { List, ListItem, ListItemText, IconButton, Button, Typography } from '@mui/material';
 import { Add, Remove, Delete } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
 
 function Cart() {
-  const cartId  = 'pU6YrRuSDYwBchVp2p68';
-  const [cartItems, setCartItems] = useState([]);
-  const navigate = useNavigate();
+    const cartId = 'pU6YrRuSDYwBchVp2p68';
+    const [cartItems, setCartItems] = useState([]);
+    const { setNumberOfItemsInCart } = useContext(CartQuantity);
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchCartItems = async () => {
-      try {
-        const { data } = await axios.get(`http://localhost:9000/cart/${cartId}`);
-        setCartItems(data.items);
-      } catch (error) {
-        console.error(`Error fetching cart: ${error}`);
-      }
+    useEffect(() => {
+        const fetchCartItems = async () => {
+            try {
+                const { data } = await axios.get(`http://localhost:9000/cart/${cartId}`);
+                setCartItems(data.items);
+                let quantity = 0;
+                data.items.forEach(item => {
+                    quantity += item.quantity;
+                });
+                setNumberOfItemsInCart(quantity);
+            } catch (error) {
+                console.error(`Error fetching cart: ${error}`);
+            }
+        };
+
+        fetchCartItems();
+    }, [cartId, setNumberOfItemsInCart]);
+
+    const deleteItem = async (itemId) => {
+        try {
+            await axios.delete(`http://localhost:9000/cart/${cartId}/${itemId}`);
+            setCartItems(cartItems.filter(item => item.id !== itemId));
+            let quantity = 0;
+            cartItems.forEach(item => {
+                if (item.id !== itemId) {
+                    quantity += item.quantity;
+                }
+            });
+            setNumberOfItemsInCart(quantity);
+        } catch (error) {
+            console.error(`Error deleting item: ${error}`);
+        }
     };
 
-    fetchCartItems();
-  }, [cartId]);
+    const updateQuantity = async (itemId, newQuantity) => {
+        if (newQuantity >= 0) {
+            try {
+                const { data } = await axios.patch(`http://localhost:9000/cart/${cartId}/${itemId}`, { quantity: newQuantity });
+                setCartItems(cartItems.map(item => item.id === itemId ? data : item));
+                let quantity = 0;
+                cartItems.forEach(item => {
+                    quantity += (item.id === itemId ? newQuantity : item.quantity);
+                });
+                setNumberOfItemsInCart(quantity);
+            } catch (error) {
+                console.error(`Error updating quantity: ${error}`);
+            }
+        }
+    };
 
-  const deleteItem = async (itemId) => {
-    try {
-      await axios.delete(`http://localhost:9000/cart/${cartId}/${itemId}`);
-      setCartItems(cartItems.filter(item => item.id !== itemId));
-    } catch (error) {
-      console.error(`Error deleting item: ${error}`);
-    }
-  };
-
-  const updateQuantity = async (itemId, newQuantity) => {
-    if (newQuantity >= 0) {
-      try {
-        const { data } = await axios.patch(`http://localhost:9000/cart/${cartId}/${itemId}`, { quantity: newQuantity });
-        setCartItems(cartItems.map(item => item.id === itemId ? data : item));
-      } catch (error) {
-        console.error(`Error updating quantity: ${error}`);
-      }
-    }
-  };
-
-  return (
-    <div>
-      <Typography variant="h4">Your Cart</Typography>
-      <List>
-        {cartItems.map(item => (
-          <ListItem key={item.id}>
-            <ListItemText primary={item.name} secondary={`Price: $${item.price}`} />
-            <IconButton onClick={() => updateQuantity(item.id, item.quantity - 1)}><Remove /></IconButton>
-            <Typography>{item.quantity}</Typography>
-            <IconButton onClick={() => updateQuantity(item.id, item.quantity + 1)}><Add /></IconButton>
-            <IconButton onClick={() => deleteItem(item.id)}><Delete /></IconButton>
-          </ListItem>
-        ))}
-      </List>
-      <Button variant="contained" color="primary" onClick={() => navigate('/checkout')}>Proceed to Checkout</Button>
-    </div>
-  );
+    return (
+        <div>
+            <Typography variant="h4">Your Cart</Typography>
+            <List>
+                {cartItems.map(item => (
+                    <ListItem key={item.id}>
+                        <ListItemText primary={item.name} secondary={`Price: $${item.price}`} />
+                        <IconButton onClick={() => updateQuantity(item.id, item.quantity - 1)}><Remove /></IconButton>
+                        <Typography>{item.quantity}</Typography>
+                        <IconButton onClick={() => updateQuantity(item.id, item.quantity + 1)}><Add /></IconButton>
+                        <IconButton onClick={() => deleteItem(item.id)}><Delete /></IconButton>
+                    </ListItem>
+                ))}
+            </List>
+            <Button variant="contained" color="primary" onClick={() => navigate('/checkout')}>Proceed to Checkout</Button>
+        </div>
+    );
 }
 
 export default Cart;
