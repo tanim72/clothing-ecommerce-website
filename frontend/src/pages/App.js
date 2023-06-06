5fb5b2dcdf3472ab392bffbd4188cc4d84964442
averydossman <averydossman@gmail.com>
2023-06-06 16:49:36 -0400

    cart quantity updates automatically

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './navbar/Navbar';
 import Cart from './cart/Cart';
+import axios from 'axios';
+import CartQuantity from './cart/CartQuantity';
 
 function App() {
+  const cartId = 'pU6YrRuSDYwBchVp2p68';
+  const [numberOfItemsInCart, setNumberOfItemsInCart] = useState(0);
+
+  useEffect(() => {
+    const fetchCartItems = async () => {
+      try {
+        const { data } = await axios.get(`http://localhost:9000/cart/${cartId}`);
+        let quantity = 0;
+        data.items.forEach(item => {
+          quantity += item.quantity;
+        });
+        setNumberOfItemsInCart(quantity);
+      } catch (error) {
+        console.error(`Error fetching cart: ${error}`);
+      }
+    };
+
+    fetchCartItems();
+  }, []);
+
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        {/* Other components
+      <CartQuantity.Provider value={{ numberOfItemsInCart, setNumberOfItemsInCart }}>
+        <Navbar />
+        <Routes>
+          {/* Other components
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} /> */}
-        <Route path="/cart/:cartId" element={<Cart />} />
-      </Routes>
+          <Route path="/cart/:cartId" element={<Cart />} />
+        </Routes>
+      </CartQuantity.Provider>
     </Router>
   );
 }
